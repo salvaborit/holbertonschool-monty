12e049e8255ae77f911c9b377c679a55a308f949
Salvador Borit <5084@holbertonschool.com>
2022-08-16 16:40:41 -0300

    fixed read lines from file

--- monty.c
+++ monty.c
@@ -2,10 +2,42 @@
 
 /**
  * main - monty bitcode interpreter main execution
- * 
+ * @ac: argument count
+ * @av: arguments to command line
+ * Return: 0 on success
  */
 
-int main(void)
+int main(int ac, char *av[])
 {
-    
+	FILE *fp = NULL;
+	char *line = NULL, *got = NULL;
+
+	/* if arg count is wrong */
+	if (ac != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+
+	/* if file opening fails */
+	fp = fopen(av[1], "r");
+	if (!fp)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", av[1]);
+		exit(EXIT_FAILURE);
+	}
+
+	do {
+		line = malloc(1024);
+		if (!line)
+			{
+				fprintf(stderr, "Error: malloc failed\n");
+				exit(EXIT_FAILURE);
+			}
+		got = fgets(line, 1024, fp);
+		printf("%s", line);
+	} while (got);
+
+	fclose(fp);
+	return(1);
 }
--- monty.h
+++ monty.h
@@ -3,6 +3,8 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
 
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
