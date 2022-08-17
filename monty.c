f2fcd5c6672ae1a00fb3fc1518e0022e028caccd
Salvador Borit <5084@holbertonschool.com>
2022-08-17 20:26:36 -0300

    test on sandbox

--- monty.c
+++ monty.c
@@ -2,42 +2,61 @@
 
 /**
  * main - monty bitcode interpreter main execution
- * @ac: argument count
- * @av: arguments to command line
- * Return: 0 on success
+ * @ac: number of args passed to command line
+ * @av: array containing args passed to command line
+ * Return: EXIT_SUCCESS or EXIT_FAILURE
  */
 
 int main(int ac, char *av[])
 {
+/*	stack_t *head = NULL; */
 	FILE *fp = NULL;
-	char *line = NULL, *got = NULL;
+	char line[1023], lineAux[1023], *delims = " \t\n", *token = NULL;
+	char **lineArr = NULL;
+	int i;
 
-	/* if arg count is wrong */
 	if (ac != 2)
 	{
 		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
 
-	/* if file opening fails */
 	fp = fopen(av[1], "r");
 	if (!fp)
 	{
 		fprintf(stderr, "Error: Can't open file %s\n", av[1]);
 		exit(EXIT_FAILURE);
 	}
+	
+	while (fgets(line, 1024, fp) != NULL) 
+	{
+		lineArr = malloc(1024);
+		if (!lineArr)
+		{
+			fprintf(stderr, "Error: malloc failed");
+			exit(EXIT_FAILURE);
+		}
 
-	do {
-		line = malloc(1024);
-		if (!line)
-			{
-				fprintf(stderr, "Error: malloc failed\n");
-				exit(EXIT_FAILURE);
-			}
-		got = fgets(line, 1024, fp);
-		printf("%s", line);
-	} while (got);
+		strcpy(lineAux, line);
+		token = strtok(lineAux, delims);
+		for (i = 0; token; i++)
+		{
+			lineArr[i] = strdup(token);
+			token = strtok(NULL, delims);
+		}
+		lineArr[i] = NULL;
+
+		while (lineArr[i])
+			printf("%s ", lineArr[i++]);
+
+		if (lineArr)
+			free(lineArr);
+		lineArr = NULL;
+	}
 
 	fclose(fp);
 	return(1);
 }
+/*
+void (*opcode(char *s))()
+*/
