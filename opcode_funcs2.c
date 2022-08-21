eeeba2ae3f17b593a60ed5e594df30ea37dce812
Mateo <mbonino1810@gmail.com>
2022-08-21 00:57:38 -0300

    swap func

--- opcode_funcs2.c
+++ opcode_funcs2.c
@@ -20,7 +20,7 @@ void op_swap(stack_t **stack, unsigned int line_number)
 	/*if (stackSize < 2)*/
 	if (i < 2)
 	{
-		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
+		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	(*stack) = (*stack)->next;
